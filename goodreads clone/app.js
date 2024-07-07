bfa02a13e7979743771b32e4aa016cd33829b9e0
Tim Cafarella <tcaf.351@gmail.com>
2024-07-08 07:58:33 +1000

    added spinner to load while fetching api then display app after api is fetched

--- goodreads clone/app.js
+++ goodreads clone/app.js
@@ -1,22 +1,55 @@
-// api
-const apiUrl = 'https://openlibrary.org/search.json';
+const spinner = document.querySelector('.loader');
+const app = document.querySelector('#app');
 
-// fetch api
-const fetchApi = async (url) => {
-    try {
-        const response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+const bookTitle = document.querySelector('.book-title');
+const authorName = document.querySelector('.author-name');
 
-        else {
-            const apiResults = await response.json();
-            console.log(apiResults);
-        }
 
-    } catch (error) {
-        console.error('Error fetching API:', error);
-    }
+const showSpinner = () => {
+        spinner.classList.remove('hidden')
+        spinner.classList.remove('none')
+        console.log('show spinner');
 };
-fetchApi(apiUrl)
+
+const hideSpinner = () => {
+        spinner.classList.add('hidden');
+        spinner.classList.remove('inline-block');
+        console.log('hide spinner');
+};
+
+const toggleApp = () => {
+    app.classList.remove('hidden');
+};
+
+
+
+// api
+const apiUrl = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings';
+
+window.onload = () => {
+    let fetchApi = async (url) => {
+        try {
+            showSpinner(); // Show spinner when starting to fetch API
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            } else {
+                const data = await response.json();
+                console.log(data);
+                
+                // const { title, author_name } = data.docs[0];
+                // bookTitle.textContent = title;
+                // authorName.textContent = author_name;
+            }
+        } catch (error) {
+            console.error('Error fetching API:', error);
+        } finally {
+            hideSpinner(); // Hide spinner after fetch is complete
+            toggleApp();
+        }
+    };
+
+    fetchApi(apiUrl);
+};
--- goodreads clone/tailwind.config.js
+++ goodreads clone/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        rotation: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        rotation: 'rotation 1s linear infinite',
+      },
+    },
   },
   plugins: [],
 }

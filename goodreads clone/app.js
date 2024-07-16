a5f63de77c4db536a4cea7f0eaacc2cee44d5a33
Tim Cafarella <tcaf.351@gmail.com>
2024-07-17 08:12:30 +1000

    cleaned up app and spinner functions

--- goodreads clone/app.js
+++ goodreads clone/app.js
@@ -1,34 +1,29 @@
-const spinner = document.querySelector('.loader');
-const app = document.querySelector('#app');
+const spinner = document.querySelector('.loader'); // get spinner
+const app = document.querySelector('#app'); // get app
 
 
-const bookTitle = document.querySelector('.book-title');
-const authorName = document.querySelector('.author-name');
-
-
-const showSpinner = () => {
+const showSpinner = () => { // show spinner
         spinner.classList.remove('hidden')
-        spinner.classList.remove('none')
-        console.log('show spinner');
 };
 
-const hideSpinner = () => {
+const hideSpinner = () => { // hide spinner
         spinner.classList.add('hidden');
-        spinner.classList.remove('inline-block');
-        console.log('hide spinner');
 };
 
-const toggleApp = () => {
+const toggleApp = () => { // removes hidden class from the app to show app
     app.classList.remove('hidden');
 };
 
+
+const bookTitle = document.querySelector('.book-title'); // get book's title
+const authorName = document.querySelector('.author-name'); // get authors name
 
 
 // api
 const apiUrl = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings';
 
 window.onload = () => {
-    let fetchApi = async (url) => {
+    let fetchApi = async (url) => { // fetch api onLoad of app opening
         try {
             showSpinner(); // Show spinner when starting to fetch API
             const response = await fetch(url);
@@ -39,7 +34,8 @@ window.onload = () => {
                 const data = await response.json();
                 console.log(data);
                 
-                const { title, author_name } = data.docs[0]; // set the book title and author name from api
+                const { title, author_name, number_of_pages_median
+                } = data.docs[0]; // set the book title and author name from api
 
                 bookTitle.textContent = title; // change the element text to be the title
                 authorName.textContent = author_name[0]; // change the element text to be the author name
@@ -48,9 +44,28 @@ window.onload = () => {
             console.error('Error fetching API:', error);
         } finally {
             hideSpinner(); // Hide spinner after fetch is complete
-            toggleApp();
+            toggleApp(); // show app
         }
     };
 
     fetchApi(apiUrl);
 };
+
+
+// calculate percentage of way through book
+
+
+// search book
+
+
+
+// add book to want to read
+
+
+
+
+// add book to read
+
+
+
+// scan barcode of book to bring up modal

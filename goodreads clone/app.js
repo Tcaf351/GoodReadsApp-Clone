5e4707d7ee244fe4b6774b96f6fdc7fbdb3aea65
Tim Cafarella <tcaf.351@gmail.com>
2024-07-28 17:44:50 +1000

    added modal when user clicks on update progress. User can enter a page number they are up to and will calculate a percentage onclick on done button

--- goodreads clone/app.js
+++ goodreads clone/app.js
@@ -1,6 +1,7 @@
-const spinner = document.querySelector('.loader'); // get spinner
 const app = document.querySelector('#app'); // get app
 
+
+const spinner = document.querySelector('.loader'); // get spinner
 
 const showSpinner = () => { // show spinner
         spinner.classList.remove('hidden')
@@ -23,32 +24,32 @@ const authorName = document.querySelector('.author-name'); // get authors name
 const apiUrl = 'https://openlibrary.org/search.json?q=the+lord+of+the+rings';
 
 window.onload = () => {
-    // let fetchApi = async (url) => { // fetch api onLoad of app opening
-    //     try {
-    //         showSpinner(); // Show spinner when starting to fetch API
-    //         const response = await fetch(url);
+    let fetchApi = async (url) => { // fetch api onLoad of app opening
+        try {
+            showSpinner(); // Show spinner when starting to fetch API
+            const response = await fetch(url);
 
-    //         if (!response.ok) {
-    //             throw new Error(`HTTP error! status: ${response.status}`);
-    //         } else {
-    //             const data = await response.json();
-    //             console.log(data);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            } else {
+                const data = await response.json();
+                console.log(data);
                 
-    //             const { title, author_name, number_of_pages_median
-    //             } = data.docs[0]; // set the book title and author name from api
+                const { title, author_name, number_of_pages_median } = data.docs[0]; // set the book title and author name from api
 
-    //             bookTitle.textContent = title; // change the element text to be the title
-    //             authorName.textContent = author_name[0]; // change the element text to be the author name
-    //         }
-    //     } catch (error) {
-    //         console.error('Error fetching API:', error);
-    //     } finally {
-    //         hideSpinner(); // Hide spinner after fetch is complete
+                bookTitle.textContent = title; // change the element text to be the title
+                authorName.textContent = author_name[0]; // change the element text to be the author name
+                bookPageCount = number_of_pages_median;
+            }
+        } catch (error) {
+            console.error('Error fetching API:', error);
+        } finally {
+            hideSpinner(); // Hide spinner after fetch is complete
             toggleApp(); // show app
-    //     }
-    // };
+        }
+    };
 
-    // fetchApi(apiUrl);
+    fetchApi(apiUrl);
 };
 
 
@@ -57,6 +58,7 @@ const updateProgressButton = document.getElementsByTagName('button')[3];
 
 // togggle modal
 const modal = document.querySelector('.modal'); // get modal
+const overlay = document.querySelector('.overlay') // get modal overlay
 
 const modalDoneButton = document.getElementsByTagName('button')[1]; // get done button to close modal
 const modalCancelButton = document.getElementsByTagName('button')[0]; // get cancel button to close modal
@@ -64,11 +66,13 @@ const modalCancelButton = document.getElementsByTagName('button')[0]; // get can
 // open model
 const openModal = () => {
     modal.classList.remove('hidden');
+    overlay.classList.remove("hidden");
 };
 
 // close modal
 const closeModal = () => {
     modal.classList.add('hidden');
+    overlay.classList.add("hidden");
 };
 
 // open modal event listener
@@ -78,13 +82,38 @@ updateProgressButton.addEventListener('click', openModal);
 modalCancelButton.addEventListener('click', closeModal);
 
 // done button on modal also closes button
-modalDoneButton.addEventListener('click', closeModal);
+modalDoneButton.addEventListener('click', () => {
+    // close modal function but updates book percentage when done
+    modal.classList.add('hidden');
+    overlay.classList.add("hidden");
+    percentage.textContent = userBookPercentage;
+});
+
+// close modal when user clicks outside of modal container
+overlay.addEventListener('click', closeModal);
 
 
 // done button fires function that closes modal, updates percentage if update has been entered else no change in percentage
 
 
-// calculate percentage of way through book
+// get input where user enters the page they're up to
+const pageNumberInput = document.querySelector('#modal-user-page-number');
+const percentage = document.querySelector('#percentage');
+let bookPageCount = 0; // set an initial value for the book percentage to work off
+let userBookPercentage = 0;
+
+pageNumberInput.addEventListener('input', (e) => {
+    // get value of input
+    const inputValue = e.target.value;
+
+    // convert to a number
+    const numberValue = parseInt(inputValue);
+
+    // calculate percentage of way through book 
+    if (!isNaN(numberValue)) {
+        userBookPercentage = Math.floor((numberValue / bookPageCount) * 100);
+    }
+});
 
 
 // search book

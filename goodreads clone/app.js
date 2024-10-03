6f17414af409d71cec8d8c11ea1437a54a582f11
Tim Cafarella <tcaf.351@gmail.com>
2024-10-04 07:34:19 +1000

    added else clause to the if statement to adding a book to localStorage. If book already exists, triggers an error message to let user know book already exists.

--- goodreads clone/app.js
+++ goodreads clone/app.js
@@ -160,14 +160,16 @@ dropdown.addEventListener('change', function() {
         subtitle: bookSubTitle.textContent,
         cover: bookCover.currentSrc,
         description: bookDescription.textContent,
-        author: authorName.textContent, // need to remove 'by'
-        publisher: bookPublisher.textContent,
+        author: authorName.textContent.slice(3),
+        publisher: bookPublisher.textContent.slice(19),
         rating: ratingAverage.textContent
     };
-    console.log(bookData);
 
     // switch case for dropdown options. Convert data to string for localStorage
     const addToLocalStorage = (key, bookData) => {
+        const errorMessageContainer = document.querySelector('#message-container');
+        errorMessageContainer.textContent = ''; // clears previous message
+
         // Retrieve existing books for the selected status (change to json) & if nothing exists then enter empty array
         let storedBooks = JSON.parse(localStorage.getItem(key)) || [];
 
@@ -179,27 +181,38 @@ dropdown.addEventListener('change', function() {
 
             // Save the updated array back to local storage
             localStorage.setItem(key, JSON.stringify(storedBooks));
+            } else {
+                const errorMessage = document.createElement('span');
+                errorMessage.textContent = `Sorry ${bookData.title} already exists in ${key}.`
+                errorMessageContainer.append(errorMessage);
             }
         }
+        
 
     const dropdownSubmitButton = document.querySelector('#dropdown-button-submit');
     dropdownSubmitButton.addEventListener('click', () => {
+        const errorMessageContainer = document.querySelector('#message-container');
+        const errorMessage = document.createElement('span');
         switch (selectedOption) {
             case 'want to read':
-                addToLocalStorage('want to read', bookData);
+                console.log(localStorage.getItem(bookData));
+                // if book is already in localStorage send error, 'sorry book already exists'
+                    addToLocalStorage('want to read', bookData);
                 break;
             
             case 'currently reading':
+                // if book is already in localStorage send error, 'sorry book already exists'
                 addToLocalStorage('currently reading', bookData);
                 break;
     
             case 'read':
+                // if book is already in localStorage send error, 'sorry book already exists'
                 addToLocalStorage('read', bookData);
                 break;
         
             default:
-                const errorMessageContainer = document.querySelector('#message-container');
-                const errorMessage = document.createElement('span');
+                // const errorMessageContainer = document.querySelector('#message-container');
+                // const errorMessage = document.createElement('span');
                 errorMessage.textContent = 'No valid option selected. Please try again';
                 errorMessageContainer.append(errorMessage);
                 break;

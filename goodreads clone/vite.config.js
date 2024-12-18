575e93223aaf86402b6e5b6a5b023523aacfd254
Tim Cafarella <tcaf.351@gmail.com>
2024-12-19 07:11:59 +1100

    vite config file resolved

--- goodreads clone/vite.config.js
+++ goodreads clone/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
       input: {
         main: './index.html', 
         read: './read.html',
-        bookDetails: './bookDetails.html',
+        bookDetails: './book-details.html',
         wantToRead: './wantToRead.html',
       }
     }
